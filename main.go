53f688d406fdab41128f0e4fef8ce512c619c1d3
Mohammad Shoaib <shoaib.khan20@outlook.com>
2022-08-30 10:53:34 -0700

    Removed unnecessary code and added docs

--- main.go
+++ main.go
@@ -19,6 +19,10 @@ import (
 
 var _spd *speechd.SpeechdSession = nil
 
+/**
+* Description: Connects with the speechd socket. If no screen reader is activated, it fails to connect on the first try although if we again try to initialize after 3-4 seconds, it connects successfully.
+* Return:      1 if successful and -1 if unsuccessful.
+*/
 //export Initialize
 func Initialize() int {
 	spd, err := speechd.Open()
@@ -31,6 +35,12 @@ func Initialize() int {
 	return 1
 }
 
+/**
+* Description: Speaks the given text.
+* Parameters:  text = the text to speak. Important!! string variable is accessed in other languages in a special way, refer to https://github.com/vladimirvivien/go-cshared-examples for examples
+*              interrupt = whether to cancel the previous speech or not.
+* Return:      1 if successful and -1 if unsuccessful.
+*/
 //export Speak
 func Speak(text string, interrupt bool) int {
 	if _spd == nil {
@@ -51,6 +61,10 @@ func Speak(text string, interrupt bool) int {
 	return 1
 }
 
+/**
+* Description: Disconnects with the speechd socket.
+* Return:      1 if successful and -1 if unsuccessful.
+*/
 //export Close
 func Close() int {
 	if _spd == nil {
@@ -59,13 +73,4 @@ func Close() int {
 
 	_spd.Close()
 	return 1
-}
-
-func main() {
-	Initialize()
-
-	Speak("131", false)
-	Speak("shoaib", true)
-
-	Close()
 }
